439cd37a8603710a75a60018285868827a35e650
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-06-08 16:09:26 +0900

    view model

---
+++ mvcsample1/Controllers/EmployeeController.cs
@@ -0,0 +1,51 @@
+﻿using mvcsample1.Models;
+using mvcsample1.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcsample1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Details()
+        {
+            //Employee Basic Details
+            Employee employee = new Employee()
+            {
+                EmployeeId = 101,
+                Name = "Dillip",
+                Gender = "Male",
+                Salary = 10000,
+            };
+            //Employee Address
+            Address address = new Address()
+            {
+                AddressId = 1001,
+                City = "Bhubaneswar",
+                State = "Odisha",
+                Country = "India",
+                Pin = "755019"
+            };
+            //Creating the View model
+            EmployeeDetailsViewModel employeeDetailsViewModel = new EmployeeDetailsViewModel()
+            {
+                Employee = employee,
+                Address = address,
+                PageTitle = "Employee Details Page",
+                PageHeader = "Employee Details",
+            };
+
+            //Pass the employeeDetailsViewModel to the view
+            return View(employeeDetailsViewModel);
+        }
+    }
+}
---
+++ mvcsample1/ViewModel/EmployeeDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using mvcsample1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvcsample1.ViewModel
+{
+    public class EmployeeDetailsViewModel
+    {
+        public Employee Employee { get; set; }
+        public Address Address { get; set; }
+        public string PageTitle { get; set; }
+        public string PageHeader { get; set; }
+    }
+}
